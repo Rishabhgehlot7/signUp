1a5cd7a76af7096d503dbd5217349e2dfd75c578
rishabh gehlot <rishabh3x@gmail.com>
2023-06-05 22:54:43 +0530

    modification 1.2

--- script.js
+++ script.js
@@ -1,134 +1,167 @@
-// <-------------------------------------------Dark mode------------------------------------------->
-
-
 const toggleCheckbox = document.querySelector('input[name="switch"]');
-
 const root = document.documentElement;
 
 function enableDarkMode() {
-    document.body.style.backgroundColor = "black"
-    document.body.style.color = "white"
-    document.body.getElementsByClassName("formPage")[0].style.backgroundColor = " rgb(12, 12, 12)"
-    document.body.getElementsByClassName("formPage")[0].style.color = "white"
+  root.classList.add('dark-mode');
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+  document.body.getElementsByClassName("formPage")[0].style.backgroundColor = "rgb(12, 12, 12)";
+  document.body.getElementsByClassName("formPage")[0].style.color = "white";
 
-    localStorage.setItem('darkModeEnabled', true);
+  localStorage.setItem('darkModeEnabled', true);
 }
 
 function disableDarkMode() {
-    root.classList.remove('dark-mode');
-
-    document.body.style.backgroundColor = "skyblue"
-    document.body.style.color = "black"
-    document.body.getElementsByClassName("formPage")[0].style.backgroundColor = "white"
-    document.body.getElementsByClassName("formPage")[0].style.color = "black"
-    document.body.getElementsByClassName("darkinput")[0].style.color = "black"
-    localStorage.setItem('darkModeEnabled', false);
+  root.classList.remove('dark-mode');
+  document.body.style.backgroundColor = "skyblue";
+  document.body.style.color = "black";
+  document.body.getElementsByClassName("formPage")[0].style.backgroundColor = "white";
+  document.body.getElementsByClassName("formPage")[0].style.color = "black";
+  document.body.getElementsByClassName("darkinput")[0].style.color = "black";
+  localStorage.setItem('darkModeEnabled', false);
 }
 
 function toggleDarkMode() {
-    if (toggleCheckbox.checked) {
-        enableDarkMode();
-    } else {
-        disableDarkMode();
-    }
+  if (toggleCheckbox.checked) {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
 }
 
 const darkModeEnabled = localStorage.getItem('darkModeEnabled');
 
 if (darkModeEnabled && darkModeEnabled !== 'false') {
-    toggleCheckbox.checked = true;
-    enableDarkMode();
+  toggleCheckbox.checked = true;
+  enableDarkMode();
 } else {
-    toggleCheckbox.checked = false;
-    disableDarkMode();
+  toggleCheckbox.checked = false;
+  disableDarkMode();
 }
 
 toggleCheckbox.addEventListener('change', toggleDarkMode);
 
-// <-------------------------------------------form validation------------------------------------------->
-
-
 function validateForm() {
-    var returnval = true;
-    // username
-    var name = document.forms['myForm']["fname"].value;
-    if (name.length <= 5 || name.length >= 20) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Invalid Username!',
-            text: 'Length should be between 5 to 20!',
-        })
-        returnval = false;
-    }
-    //phone number
-    var phone = document.forms['myForm']["fphone"].value;
-    if (phone.length != 10) {
-        Swal.fire({
-            icon: 'error',
-            title: 'Invalid Phone Number!',
-            text: 'Phone number Must be 10 digits',
-        })
-        returnval = false;
-    }
+  let valid = true;
 
+  // Username
+  const name = document.forms['myForm']["fname"].value;
+  if (name.length < 5 || name.length > 20) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Username!',
+      text: 'Length should be between 5 and 20 characters!',
+    });
+    valid = false;
+    return; // Stop validation for other fields
+  }
 
-    // password
-    var password = document.forms['myForm']["fpass"].value;
-    function validatePassword(password) {
-        if (password.length < 7 || password.length > 12) {
-            return false;
-        }
+  // Phone number
+  const phoneNumber = document.forms['myForm']["fphone"].value;
+  if (phoneNumber.length !== 10) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Phone Number!',
+      text: 'Phone number must be 10 digits',
+    });
+    valid = false;
+    return; // Stop validation for other fields
+  } else if (/^0+$/.test(phoneNumber)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Phone Number!',
+      text: 'Phone number must not consist of all zeros',
+    });
+    valid = false;
+    return; // Stop validation for other fields
+  }
 
-        if (!/[a-z]/.test(password)) {
-            return false;
-        }
+  // Password
+  const password = document.forms['myForm']["fpass"].value;
+  if (password.length < 7 || password.length > 12 || !/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/\d/.test(password)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Password!',
+      text: 'Password must be between 7 and 12 characters and include at least one lowercase letter, one uppercase letter, and one digit',
+    });
+    valid = false;
+    return; // Stop validation for other fields
+  }
 
-        if (!/[A-Z]/.test(password)) {
-            return false;
-        }
+  // Email
+  const email = document.forms['myForm']["Email"].value;
+  if (!validateEmail(email)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Email Address!',
+      text: 'Please enter a valid email address with a popular domain (e.g., Gmail, Yahoo, Hotmail)',
+    });
+    valid = false;
+    return; // Stop validation for other fields
+  }
 
-        if (!/\d/.test(password)) {
-            return false;
-        }
+  // Date of Birth
+  const dateOfBirth = document.forms['myForm']["DOB"].value;
+  if (!validateDateOfBirth(dateOfBirth)) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Date of Birth!',
+      text: 'Please enter a valid date of birth in the format dd-mm-yyyy, and the applicant must be at least 10 years old',
+    });
+    valid = false;
+    return; // Stop validation for other fields
+  }
 
-        return true;
-    }
+  Swal.fire({
+    icon: 'success',
+    title: 'Form Submitted!',
+    text: 'Thank you for submitting the form.',
+  });
 
-    if (validatePassword(password)) {
-        console.log("Password is valid.");
-    } else {
-        console.log("Password is invalid.");
-        Swal.fire({
-            icon: 'error',
-            title: 'Invalid password!',
-            text: 'Password must be between 7 to 12, A lowercase letter, A upper case letter, A number'
-
-        })
-        returnval = false;
-    }
-
-
-    // email
-    var email = document.forms['myForm']["Email"].value;
-    function validateEmail(email) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    }
-
-    const isValidEmail = validateEmail(email);
-
-    if (isValidEmail) {
-        console.log("Valid email address");
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Invalid email address!',
-            text: 'Valid Email Syntax is "Invalid email address!"',
-        })
-        returnval = false;
-    }
-
-
-    return returnval;
+  return valid;
 }
 
+function validateEmail(email) {
+  const pattern = /^[^\s@]+@(gmail\.com|yahoo\.com|hotmail\.com)$/i;
+  return pattern.test(email);
+}
+
+function validateDateOfBirth(dateString) {
+  const parts = dateString.split("-");
+  if (parts.length !== 3) return false;
+
+  const day = parseInt(parts[0], 10);
+  const month = parseInt(parts[1], 10) - 1;
+  const year = parseInt(parts[2], 10);
+
+  if (isNaN(day) || isNaN(month) || isNaN(year)) return false;
+
+  const dateOfBirth = new Date(year, month, day);
+  if (dateOfBirth.toString() === 'Invalid Date') return false;
+
+  const currentDate = new Date();
+  currentDate.setHours(0, 0, 0, 0); // Set time to 00:00:00
+
+  if (dateOfBirth > currentDate) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Invalid Date of Birth!',
+      text: 'Future dates are not allowed',
+    });
+    return false;
+  }
+
+  const ageDiff = currentDate.getFullYear() - dateOfBirth.getFullYear();
+  const monthDiff = currentDate.getMonth() - dateOfBirth.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < dateOfBirth.getDate())) {
+    return ageDiff - 1 >= 10;
+  }
+
+  return ageDiff >= 10;
+}
+
+document.forms['myForm'].addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
